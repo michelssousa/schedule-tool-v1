afd486ba68b5120aa34c73e8c9ddfb1b758b91a5
michel <michelsousa7@gmail.com>
2022-05-15 07:50:40 -0300

    config css base

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,33 @@
+import React from "react";
+
 import { ChakraProvider } from "@chakra-ui/react";
+import { Global, css } from "@emotion/react";
 import { AppProps } from "next/app";
 
 import { ScheduleProvider } from "~/core";
 import theme from "~/styles/theme";
 
+const globalStyles = css`
+  * {
+    font-size: 62.5%;
+  }
+  ,
+  html {
+    scroll-behavior: smooth;
+  }
+  ,
+  #__next {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+`;
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ScheduleProvider>
       <ChakraProvider resetCSS theme={theme}>
+        <Global styles={globalStyles} />
         <Component {...pageProps} />
       </ChakraProvider>
     </ScheduleProvider>
